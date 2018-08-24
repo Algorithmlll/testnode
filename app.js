6725edb356eaca10d70f27f936a33e6368b1a846
Same Chinnawat <Chinnawat@livinginsider.com>
2018-08-25 00:28:49 +0700

    remove http createServer

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const app = express()
-var http = require('http');
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-// app.listen(3000, () => {
-//   console.log('Start server at port 3000.')
-// })
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Apache!\n');
-}).listen(3000, '127.0.0.1');
+app.listen(3000, () => {
+  console.log('Start server at port 3000.')
+})
