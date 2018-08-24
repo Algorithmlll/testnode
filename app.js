c12eaf73b57a1a1e13fe90a7026a9c065ef674d1
Same Chinnawat <Chinnawat@livinginsider.com>
2018-08-24 23:58:15 +0700

    http createServer

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+var http = require('http');
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000, () => {
-  console.log('Start server at port 3000.')
-})
+// app.listen(3000, () => {
+//   console.log('Start server at port 3000.')
+// })
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Apache!\n');
+}).listen(3000, '127.0.0.1');
